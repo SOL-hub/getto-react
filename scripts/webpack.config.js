2f3ba2578a9742b7eec5cd4de80d8146e3caf95b
PARKSOL <thfdl0317@naver.com>
2024-06-24 02:29:07 +0900

    feat : babel, webpack 설정

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,49 +1,64 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') //번들링이 일어날 때 css파일을 별도의 파일로 호환하겠다.
 
 module.exports = {
-    entry: './src/index.jsx',
-    resolve: {
-        extensions: ['.js', '.jsx'] // 번들링에 사용될 파일 확장자 설정
+  entry: "./src/index.jsx",
+  resolve: {
+    extensions: [".js", ".jsx"], // 번들링에 사용될 파일 확장자 설정
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"), // 현재 경로의 'dist' 폴더에 결과물 저장
+    filename: "static/js/[name].[contenthash:8].js",
+    clean: true, // 빌드 시 이전 내용 덮어쓰기
+  },
+  devtool: isProduction ? false : "eval-source-map", // 디버깅을 위한 소스 맵 설정
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+    client: {
+      overlay: true, // 코드 에러 시 화면에 오버레이로 표시
+      progress: true, // 진행 상황 표시
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'), // 현재 경로의 'dist' 폴더에 결과물 저장
-        filename: 'static/js/[name].[contenthash:8].js',
-        clean: true // 빌드 시 이전 내용 덮어쓰기
-    },
-    devtool: isProduction ? false : 'eval-source-map', // 디버깅을 위한 소스 맵 설정
-    devServer: {
-        port: 3000,
-        hot: true,
-        open: true,
-        client: {
-            overlay: true, // 코드 에러 시 화면에 오버레이로 표시
-            progress: true // 진행 상황 표시
-        }
-    },
-    module: {
-        rules: [
-            {
-                oneOf: [{
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader'
-                    }
-                }]
-            }
-        ]
-    },
-    plugins: [ // Plugins -> plugins로 수정
-        isProduction ? 
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-            minify: true
-        }) :
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+            },
+          },
+          {
+            test: /\.css$/,
+            exclude : /node_modules/,
+            use: [
+                isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'
+            ]
+          }
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // Plugins -> plugins로 수정
+    isProduction
+      ? new HtmlWebpackPlugin({
+          template: "public/index.html",
+          minify: true,
         })
-    ]
+      : new HtmlWebpackPlugin({
+          template: "public/index.html",
+        }),
+        isProduction ? new MiniCssExtractPlugin({
+            linkType : false,
+            filename : '[name].[contenthash:8].css'
+        }) : undefined
+  ].filter(Boolean)
 };
